b5628111344abfa78f2dd281c3c39e402893e3b8
Dave Asay <dvasay@gmail.com>
2018-02-09 14:27:16 -0700

    minor change

--- main.rb
+++ main.rb
@@ -1,25 +1,26 @@
 require 'corlorize'
-def puts_git(cmd)
-    puts `git #{cmd} -h`
-end
+require_relative 'git'
 
-def menu 
-    puts 'MAIN MEMNU' .colorize(:cyan)
-    puts '1: Enter git command' .colorize(:cyan)
-    puts '2: Exit' .colorize(:cyan)
-    choice = gets.to_i
-    case choice
-    when 1
-        puts 'Entger git command' .colorize(:green)
-        puts_git(gets.strip)
-        menu
-    when 2 
-        exit 
-    else
-        puts 'Invalid choice'
-        menu
+class Main
+    include Git 
+
+    def menu 
+        puts 'MAIN MEMNU' .colorize(:cyan)
+        puts '1: Enter git command' .colorize(:cyan)
+        puts '2: Exit' .colorize(:cyan)
+        choice = gets.to_i
+        case choice
+        when 1
+            puts 'Entger git command' .colorize(:green)
+            puts_git(gets.strip)
+            menu
+        when 2 
+            exit 
+        else
+            puts 'Invalid choice1'
+            menu
     end
 end
 
-menu 
+Main.menu 
     
